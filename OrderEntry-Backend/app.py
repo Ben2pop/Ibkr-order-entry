58caaa6bb8ef54094456881e54296f5e92532363
Ben2pop <r.bendenoun@gmail.com>
2024-09-15 13:42:46 +0300

    initial commit

---
+++ OrderEntry-Backend/app.py
@@ -0,0 +1,130 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
+from ibkr_client import IBKRClient
+import requests
+import threading
+import time
+
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS
+
+api_key = '797222e1admsh97b6b65cb3305ebp1e200fjsncf2c020d9c27'
+
+
+def fetch_5min_candle_low(api_key, symbol):
+    print('fetching 5 min')
+    url = "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history"
+    print(symbol)
+    querystring = {"symbol": symbol, "interval": "5m","diffandsplits":"false"}  # Fetch last 1 day's 5-min interval data
+
+    headers = {
+        "x-rapidapi-key": api_key,
+        "x-rapidapi-host": "yahoo-finance15.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    print('OK')
+    if response.status_code == 200:
+        res = response.json()
+        try:
+            print(res)
+            keys = list(res['body'].keys())
+            lastkey = keys[-1]
+            lowOfLast5MinCandle = res['body'][lastkey]['low']
+            return lowOfLast5MinCandle
+        except KeyError:
+            return None
+    else:
+        return None
+
+def fetch_last_ask_price(api_key, symbol):
+    url = "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/quote"
+    querystring = {"ticker": symbol, "type": "STOCKS"}
+    headers = {
+        "x-rapidapi-key": api_key,
+        "x-rapidapi-host": "yahoo-finance15.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    if response.status_code == 200:
+        res = response.json()
+        try:
+            askPrice = res['body']['primaryData']['askPrice']
+            if askPrice == 'N/A':
+                askPrice = 0
+            else:
+                print('askPrice:   ', askPrice )
+                askPrice = float(askPrice.replace('$', '').replace(',', ''))
+                print(askPrice)
+                lowOfDay = res['body']['keyStats']['dayrange']['value'].split("-")[0].strip()
+                if lowOfDay == 'NA':
+                    lowOfDay = 0.0
+                else:
+                    lowOfDay = float(lowOfDay.replace('$', '').replace(',', ''))
+                return {'askPrice': askPrice, 'lowOfDay': lowOfDay}
+        except KeyError:
+            return None
+    else:
+        return None
+
+@app.route('/get_last_ask_price', methods=['GET'])
+def get_ask_price():
+    ticker = request.args.get('ticker')
+    timeframe = request.args.get('timeframe', '1Day')
+
+    if timeframe == '1Day':
+        if not ticker:
+            return jsonify({'error': 'Ticker is required'}), 400
+        price_data = fetch_last_ask_price(api_key, ticker)
+        if price_data is None:
+            return jsonify({'error': 'No ask price available'}), 500
+    
+        return jsonify({
+            'askPrice':price_data['askPrice'],
+            'lowOfDay': price_data['lowOfDay']
+        })
+    else:
+        print('this is 5min')
+        if not ticker:
+            return jsonify({'error': 'Ticker is required'}), 400
+        last5min = fetch_5min_candle_low(api_key, ticker)
+        price_data = fetch_last_ask_price(api_key, ticker)
+        if price_data is None:
+            return jsonify({'error': 'No ask price available'}), 500
+        return jsonify({
+            'askPrice':price_data['askPrice'],
+            'lowOfDay': last5min
+        })
+    
+# Initialize IBKR client
+
+
+@app.route('/send_order', methods=['POST'])
+def send_order():
+    data = request.json
+    ticker = data.get('ticker')
+    action = data.get('action')
+    quantity = data.get('adjustedQuantity')
+    order_type = data.get('orderType')
+    time_in_force = data.get('timeInForce')
+    stop_loss = data.get('stopLoss')
+    print('test')
+    # Initialize IBKR client and set order data
+    client = IBKRClient()
+    client.set_order_data(ticker, action, quantity, order_type, time_in_force, stop_loss)
+    print('client')
+    # Connect to TWS/IB Gateway and run the client
+    client.connect("127.0.0.1", 7497, 100)
+    print('connected')
+    client.run()
+
+    # Run the client and wait for the order to complete
+    client.run_until_complete()
+    print('finish')
+    return jsonify({'status': 'Order completed'}), 200
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ ibkr-frontend/src/App.js
@@ -0,0 +1,346 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './App.css';
+import { ThreeDots } from 'react-loader-spinner'; // Import the loader component
+import { SwipeableButton } from "react-swipeable-button";
+
+function App() {
+  const [ticker, setTicker] = useState('');
+  const [askPrice, setAskPrice] = useState('');
+  const [action, setAction] = useState('Buy');
+  const [quantity, setQuantity] = useState(0);
+  const [orderType, setOrderType] = useState('Market');
+  const [timeInForce, setTimeInForce] = useState('GTC');
+  const [investedCapital, setInvestedCapital] = useState(28000);
+  const [riskPerTrade, setRiskPerTrade] = useState(0.5);
+  const [stopLoss, setStopLoss] = useState('');
+  const [stopLossPercentage, setStopLossPercentage] = useState(''); // New state for SL(%)
+  const [takeProfit, setTakeProfit] = useState('');
+  const [lowOfDay, setLowOfDay] = useState('');
+  const [askDate, setAskDate] = useState('');
+  const [askTime, setAskTime] = useState('');
+  const [lowDate, setLowDate] = useState('');
+  const [lowTime, setLowTime] = useState('');
+  const [timeframe, setTimeframe] = useState('1Day'); // Default timeframe
+  const [sliderValue, setSliderValue] = useState(0); // New state for slider
+  const [sliderPosition, setSliderPosition] = useState(false); // New state for slider
+  const [loading, setLoading] = useState(false); // State for loader
+  const [resetKey, setResetKey] = useState(0); // To force re-render
+  const [quantityFraction, setQuantityFraction] = useState('full'); // Default selection: 'Full'
+  const [adjustedQuantity, setAdjustedQuantity] = useState(quantity); // Adjusted quantity based on button selection
+
+  const handleQuantityFractionChange = (fraction) => {
+    setQuantityFraction(fraction);
+    switch (fraction) {
+      case 'half':
+        setQuantityFraction('half');
+        setAdjustedQuantity(quantity / 2);
+        break;
+      case 'quarter':
+        setQuantityFraction('quarter');
+        setAdjustedQuantity(quantity / 4);
+        
+        break;
+      default:
+        setAdjustedQuantity(quantity);
+        break;
+    }
+  };
+
+  
+  useEffect(() => {
+    if (askPrice && stopLoss) {
+      const slPercentage = ((askPrice - stopLoss) / stopLoss * 100).toFixed(2);
+      setStopLossPercentage(slPercentage);
+    }
+  }, [askPrice, stopLoss]);
+  
+  
+  useEffect(() => {
+    if (askPrice && stopLoss && investedCapital && riskPerTrade) {
+      const calculatedQuantity = Math.ceil(
+        (investedCapital * riskPerTrade / 100) / (askPrice - stopLoss)
+      );
+      setQuantity(calculatedQuantity);
+    }
+  }, [askPrice, stopLoss, investedCapital, riskPerTrade]);
+
+  const resetButton = () => {
+    setResetKey(prevKey => prevKey + 1); // Increment key to trigger re-render
+    setSliderPosition(false); // Reset slider position state if necessary
+  };
+
+  useEffect(() => {
+    // Update adjustedQuantity when quantity or quantityFraction changes
+    switch (quantityFraction) {
+      case 'half':
+        setAdjustedQuantity(quantity / 2);
+        break;
+      case 'quarter':
+        setAdjustedQuantity(quantity / 4);
+        break;
+      default:
+        setAdjustedQuantity(quantity);
+        break;
+    }
+  }, [quantity, quantityFraction]); 
+
+  const handleGetData = async () => {
+    setLoading(true); // Show loader
+    try {
+      const response = await fetch(`http://localhost:5000/get_last_ask_price?ticker=${ticker}&timeframe=${timeframe}`);
+      const data = await response.json();
+
+      if (data.askPrice !== undefined || data.lowOfDay !== undefined) {
+        const askPrice = parseFloat(data.askPrice);
+        const lowOfDay = parseFloat(data.lowOfDay);
+        setAskPrice(askPrice);
+        setLowOfDay(lowOfDay);
+        setStopLoss(lowOfDay); // Update stop loss with the low of the day
+        const calculatedQuantity = Math.ceil(
+          (investedCapital * riskPerTrade / 100) / (askPrice - lowOfDay)
+        );
+        setQuantity(calculatedQuantity);
+        setAdjustedQuantity(calculatedQuantity)
+        setSliderPosition(false)
+        setResetKey(prevKey => prevKey + 1);
+      } else {
+        alert(data.error || 'Error fetching ask price');
+      }
+    } catch (error) {
+      alert('Failed to fetch data');
+    } finally {
+      setLoading(false); // Hide loader
+    }
+  };
+
+  const handleSLPercentageChange = (e) => {
+    const slPercentage = e.target.value;
+    setStopLossPercentage(slPercentage);
+
+    if (askPrice) {
+      const newStopLoss = askPrice - (askPrice * slPercentage / 100);
+      setStopLoss(newStopLoss.toFixed(2));
+    }
+  };
+
+
+  const sendOrder = async () => {
+    console.log('adjustedQuantity==>',adjustedQuantity)
+    if (sliderPosition !== true) return; // Don't send if slider isn't at 100
+    setLoading(true); // Show loader
+    try {
+      const response = await fetch('http://localhost:5000/send_order', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ticker,
+          action,
+          adjustedQuantity,
+          orderType,
+          timeInForce,
+          stopLoss
+        }),
+      });
+  
+      const data = await response.json();
+      console.log(data)
+      if (data.status === 'Order completed') {
+        alert('Order placed successfully!');
+      } else {
+        alert('Failed to place order: ' + data.error);
+      }
+    } catch (error) {
+      alert('Error sending order: ' + error.message);
+    } finally {
+      setLoading(false); // Hide loader
+    }
+  };
+
+  const handleActionChange = (newAction) => {
+    setAction(newAction);
+  };
+
+  // Calculate the risk per share and total risk
+  const riskPerShare = askPrice && stopLoss ? askPrice - stopLoss : 0;
+  const totalRisk = (riskPerShare * adjustedQuantity).toFixed(0);
+  const totalInvestedCapital = (adjustedQuantity * askPrice).toFixed(0);
+  const investedOnPortfolio = (totalInvestedCapital / investedCapital * 100).toFixed(2);
+  const riskPer = (((askPrice - stopLoss) / stopLoss) * 100).toFixed(2);
+
+  return (
+    <div className="app">
+      <div className="form-container">
+        <div className="order-entry-section">
+          <h3>Order Entry</h3>
+          <div className="form-group">
+            <label>Ticker:</label>
+            <input 
+              type="text" 
+              value={ticker} 
+              onChange={(e) => setTicker(e.target.value)} 
+              className="input-field ticker-input tickerInput"  /* Added class name */
+            />
+            
+            <div className="timeframe-buttons">
+              <button
+                className={`timeframe-btn ${timeframe === '1Day' ? 'active' : ''}`}
+                onClick={() => setTimeframe('1Day')}
+              >
+                1Day
+              </button>
+              <button
+                className={`timeframe-btn ${timeframe === '5Min' ? 'active' : ''}`}
+                onClick={() => setTimeframe('5Min')}
+              >
+                5Min
+              </button>
+            </div>
+            
+            <button onClick={handleGetData} className="btn marg">
+              Get Data
+            </button>
+            {loading && <ThreeDots color="#007bff" height={40} width={80} />} {/* Loader */}
+          </div>
+          <div className="form-group">
+            <label>Ask Price:</label>
+            <input 
+              type="number" 
+              value={askPrice}   
+              className="input-field"
+              onChange={(e) => setAskPrice(e.target.value)}
+              step='0.01' 
+            />
+          </div>
+          <div className="form-group">
+            <label>Stop Loss:</label>
+            <input 
+              type="number" 
+              value={stopLoss} 
+              onChange={(e) => setStopLoss(e.target.value)}
+              step='0.01' 
+              className="input-field"
+            />
+          </div>
+           {/* New SL(%) Field */}
+           <div className="form-group">
+            <label>SL(%):</label>
+            <input 
+              type="number" 
+              value={stopLossPercentage}
+              onChange={handleSLPercentageChange} 
+              step='0.01' 
+              className="input-field"
+            />
+          </div>
+          <div className="form-group">
+            <div className="action-buttons">
+              <button
+                className={`action-btn buy ${action === 'Buy' ? 'active buy-active' : ''}`}
+                onClick={() => handleActionChange('Buy')}
+              >
+                Buy
+              </button>
+              <button
+                className={`action-btn sell ${action === 'Sell' ? 'active sell-active' : ''}`}
+                onClick={() => handleActionChange('Sell')}
+              >
+                Sell
+              </button>
+            </div>
+          </div>
+          <div className="form-group">
+
+  <div className="quantity-buttons">
+    <button
+      className={`btn ${quantityFraction === 'full' ? 'active' : ''}`} // active class when full is selected
+      onClick={() => handleQuantityFractionChange('full')}
+    >
+      Full
+    </button>
+    <button
+      className={`btn ${quantityFraction === 'half' ? 'active' : ''}`} // active class when half is selected
+      onClick={() => handleQuantityFractionChange('half')}
+    >
+      1/2
+    </button>
+    <button
+      className={`btn ${quantityFraction === 'quarter' ? 'active' : ''}`} // active class when quarter is selected
+      onClick={() => handleQuantityFractionChange('quarter')}
+    >
+      1/4
+    </button>
+  </div>
+</div>
+          <div className="form-group">
+            <label>Quantity:</label>
+            <input 
+              type="number" 
+              value={adjustedQuantity} 
+              onChange={(e) => setAdjustedQuantity(e.target.value)} 
+              step='1'
+              className="input-field"
+            />
+          </div>
+          <div className="form-group">
+            <label>Order Type:</label>
+            <select 
+              value={orderType} 
+              onChange={(e) => setOrderType(e.target.value)} 
+              className="input-field"
+            >
+              <option value="Market">Market</option>
+              <option value="Limit">Limit</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Time in Force:</label>
+            <select 
+              value={timeInForce} 
+              onChange={(e) => setTimeInForce(e.target.value)} 
+              className="input-field"
+            >
+              <option value="GTC">GTC</option>
+              <option value="IOC">IOC</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div className="summary-box">
+        <h3>Order Summary</h3>
+        <ul>
+          <li>Order Type: <strong>{action}</strong></li>
+          <li># of Shares: <strong style={{ color: 'red' }}>{adjustedQuantity}</strong></li>
+          <li>Ask Price: <strong>{askPrice}</strong></li>
+          <li>Stop Loss: <strong style={{ color: 'red' }}>{stopLoss}</strong></li>
+          <li>Max Loss: <strong style={{ color: 'red' }}>{totalRisk}$</strong> - {riskPer}%</li>
+          <li>Invested Capital: {totalInvestedCapital}$ - ({investedOnPortfolio}% of Cap)</li>
+        </ul>
+       
+        <div className="w-[500px] h-[100px] bg-white">
+       <SwipeableButton
+                  onSuccess={(e) => setSliderPosition(true)}  //callback function
+                  text="Confirm order" //string 
+                  text_unlocked="Order Locked" //string
+                  key={resetKey}
+                  color="#16362d" //css hex color
+                />
+      </div>
+        <div className='form-group marg'>
+          <button 
+            onClick={sendOrder} 
+            className="btn marg" 
+            disabled={sliderPosition !== true} // Disable button if slider isn't at max value
+          >
+            Send Order
+          </button>
+          {loading && <ThreeDots color="#007bff" height={40} width={80} />} {/* Loader */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
